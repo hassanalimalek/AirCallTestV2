5a0e9d71887242bb2baf32fadf99a0b84ac368bf
Hassan Ali <hassan@turingtechnologies.org>
2022-05-18 11:26:15 +0500

    Format Fix 2

--- services/GraphQL/Mutations/Login.ts
+++ services/GraphQL/Mutations/Login.ts
@@ -1,17 +1,19 @@
-import { gql } from '@apollo/client';
-
+import { gql } from '@apollo/client'
 
 export const LOGIN = gql`
-    mutation LOGIN($username:String!,$password:String!){
-        login(input: { username: $username, password: $password })
-            {
-                access_token,
-                refresh_token,
-                user { id,username }
-            }
-}`
+  mutation LOGIN($username: String!, $password: String!) {
+    login(input: { username: $username, password: $password }) {
+      access_token
+      refresh_token
+      user {
+        id
+        username
+      }
+    }
+  }
+`
 
 // export const USER_CHECK = gql`
-//     query() 
+//     query()
 
 // `
